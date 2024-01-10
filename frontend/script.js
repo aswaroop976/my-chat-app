6763ca27f99bbbee1d4bcf2a14cc25c6f1e8c4b3
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-10 17:38:04 -0600

    moved the javascript for the frontend out of the html files

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,36 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
 
-// var socket = new WebSocket("ws://localhost:8080/ws");
+socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    socket.send("Hello, server!");
+};
 
-// socket.onopen = function (e) {
-//     console.log("[open] Connection established");
-//     socket.send("Hello, server!");
-// };
+socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+    // dynamically create message cards here
+    const div = document.createElement('div')
+    const h4 = document.createElement('h4')
+    const p = document.createElement('p')
+    const feed_div = document.querySelector('#message-feed')
+    feed_div.append(div)
+    div.setAttribute('class', 'card')
+    div.append(h4)
+    h4.append(p)
+    p.innerHTML = event.data
 
-// socket.onmessage = function (event) {
-//     console.log(`[message] Data received from server: ${event.data}`);
-// };
+};
 
-// socket.onclose = function (event) {
-//     if (event.wasClean) {
-//         console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-//     } else {
-//         console.error('[close] Connection died');
-//     }
-// };
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        console.error('[close] Connection died');
+    }
+};
 
-// socket.onerror = function (error) {
-//     console.error(`[error] ${error.message}`);
-// };
+socket.onerror = function (error) {
+    console.error(`[error] ${error.message}`);
+};
 
 
 document.getElementById('send-button').addEventListener('click', function () {
@@ -32,5 +42,3 @@ document.getElementById('send-button').addEventListener('click', function () {
         socket.send(message);
     }
 });
-
-// Additional functionality to retrieve and display messages will be added here
