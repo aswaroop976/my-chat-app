57ddaf7d72d525b0fee832985f1c397e04443449
aswaroop976 <arpan.swaroop@gmail.com>
2024-01-09 23:10:34 -0600

    Able to create html from javascript, first step in developing a dynamic webpage

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,26 @@
 
-var socket = new WebSocket("ws://localhost:8080/ws");
+// var socket = new WebSocket("ws://localhost:8080/ws");
 
-socket.onopen = function (e) {
-    console.log("[open] Connection established");
-    socket.send("Hello, server!");
-};
+// socket.onopen = function (e) {
+//     console.log("[open] Connection established");
+//     socket.send("Hello, server!");
+// };
 
-socket.onmessage = function (event) {
-    console.log(`[message] Data received from server: ${event.data}`);
-};
+// socket.onmessage = function (event) {
+//     console.log(`[message] Data received from server: ${event.data}`);
+// };
 
-socket.onclose = function (event) {
-    if (event.wasClean) {
-        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    } else {
-        console.error('[close] Connection died');
-    }
-};
+// socket.onclose = function (event) {
+//     if (event.wasClean) {
+//         console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+//     } else {
+//         console.error('[close] Connection died');
+//     }
+// };
 
-socket.onerror = function (error) {
-    console.error(`[error] ${error.message}`);
-};
+// socket.onerror = function (error) {
+//     console.error(`[error] ${error.message}`);
+// };
 
 
 document.getElementById('send-button').addEventListener('click', function () {
